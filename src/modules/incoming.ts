a2f61cb53351a977a98c161f3d232399e8c5008f
Lipa Long <lipalong@Lipas-MacBook-Pro.local>
2021-06-21 09:33:51 -0700

    adding is_keyring_locked back into incoming.ts after accidentally deleting it

--- src/modules/incoming.ts
+++ src/modules/incoming.ts
@@ -143,6 +143,15 @@ export default function incomingReducer(
             logged_in_received: true,
           };
         }
+      } else if (command === 'is_keyring_locked') {
+         success = data.success;
+         if (success) {
+           const { is_keyring_locked } = data;
+           return {
+             ...state,
+             password_status: is_keyring_locked,
+           };
+         }
       } else if (command === 'get_public_keys') {
         success = data.success;
         if (success) {
