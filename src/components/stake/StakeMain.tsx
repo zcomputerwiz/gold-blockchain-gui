ae481c9804d6e5179448e1d6655a94e820761d2b
johnfinalsrt <johnfinalsrt@gmail.com>
2021-10-08 14:59:19 +0800

    stakemain

--- src/components/stake/StakeMain.tsx
+++ src/components/stake/StakeMain.tsx
@@ -4,12 +4,14 @@ import { Box, Paper } from '@material-ui/core';
 import { Flex, Form, InputBase } from '@chia/core';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
-import StakeSelect from "./StakeSelect"
 
-import {
-  Typography,
-  Button,
-} from '@material-ui/core';
+import { useToggle } from 'react-use';
+import { Button, Divider, Menu, MenuItem, Typography } from '@material-ui/core';
+import { ExpandMore } from '@material-ui/icons';
+import { useDispatch } from 'react-redux';
+import { get_plots } from '../../modules/fullnodeMessages';
+import { isNull } from "lodash";
+
 
 const StyledInputBase = styled(InputBase)`
   min-width: 15rem;
@@ -20,19 +22,61 @@ export default function StakeMain() {
 
     type FormData = {
         amount: string;
-        publicKey: string;
     };
 
     const methods = useForm<FormData>({
         shouldUnregister: false,
         defaultValues: {
           amount: '',
-          publicKey: '',
         },
     });
 
-    async function handleSearch(values: FormData) {
+    async function handleSubmit(values: FormData) {
+      if (plotPublicKey.startsWith("---")) {
+        alert("Must select a publicKey!!!");
+        return;
+      }
 
+      let amount = values.amount;
+      let amountInt = parseInt(amount);
+      if (isNaN(amountInt) || (amountInt < 1)) {
+        alert("Must input a valid quantity!!!");
+        return;
+      }
+
+      alert(amount + plotPublicKey);
+    }
+
+
+
+    //StakeSelect
+    const [plotPublicKey, setPlotPublicKey] = useState("-----------");
+    const [values, setValues] = useState([" ", " "]);
+
+    const dispatch = useDispatch();
+    const [open, toggleOpen] = useToggle(false);
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const handleClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+      toggleOpen();
+      
+      const data = await dispatch(get_plots());
+      let plots = data.plots;
+      let publickey = plots.map((x) => {
+        return x["farmer_public_key"];
+      });
+      setValues(Array.from(new Set(publickey)));
+    };
+
+    const handleClose = () => {
+      setAnchorEl(null);
+      toggleOpen();
+    };
+
+    function handleSelect(value: string) {
+      toggleOpen();
+      setPlotPublicKey(value);
     }
 
 
@@ -43,27 +87,37 @@ export default function StakeMain() {
             xxxxxxxxxxxxxxxxxxxxxx.
             </Typography>
 
-            <Form methods={methods} onSubmit={handleSearch}>
+            <Paper elevation={0} variant="outlined">
+              <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} endIcon={<ExpandMore />}>
+                {plotPublicKey}
+              </Button>
+
+              <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={open} onClose={handleClose}>
+                {values.map((item) => (
+                  <MenuItem
+                    key={item}
+                    onClick={() => handleSelect(item)}
+                    selected={item === plotPublicKey}
+                  >
+                    {item}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Paper>
+
+            <Form methods={methods} onSubmit={handleSubmit}>
             <Paper elevation={0} variant="outlined">
                 <Flex alignItems="center" gap={1}>
-                <Box />
-                <StyledInputBase
-                    name="amount"
-                    placeholder='Amount'
-                    fullWidth
-                />
-                <StakeSelect/>
+                  <Box/>
+                  <StyledInputBase name="amount" placeholder='Amount' fullWidth/>
                 </Flex>
             </Paper>
 
-            <Flex justifyContent="flex-end" gap={1}>
-              <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-              > <Trans>Send</Trans>
-              </Button>
-            </Flex>
+            <br/>
+
+            <Button variant="contained" color="primary" type="submit"> 
+            <Trans>Send</Trans>
+            </Button>
             </Form>
 
         </Flex>
